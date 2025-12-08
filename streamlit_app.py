0e6c22b18ba4390027dd6b23a852790230f1e367
IshwariAdhau <ishwariadhau22@gmail.com>
2025-12-08 18:04:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-import pandas as pd
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -19,7 +18,7 @@ my_dataframe = session.table("smoothies.public.fruit_options") .select(col('FRUI
 #st.stop()
 
 # convert snowpark dataframe to pandas dataframe so we can use loc function
-pd_df=my_dataframe.to_pandas()
+#pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
